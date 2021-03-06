1f21fb7c67e3e9d2ecf48b72b816cbf863f6acae
Mhmd.H <dr.co0obra@hotmail.com>
2021-03-06 13:40:32 +0200

    get employee by ID done

--- controllers/projectmanagers/employees/index.js
+++ controllers/projectmanagers/employees/index.js
@@ -1,1 +1,2 @@
 export {default as getAllEmployees} from './getAll.js';
+export {default as getByID} from './getByID.js';
--- routes/generealRoutess.js
+++ routes/generealRoutess.js
@@ -1,5 +1,10 @@
-import {getAllEmployees} from '../controllers/projectmanagers/employees/index.js';
+import {
+	getAllEmployees,
+	getByID,
+} from '../controllers/projectmanagers/employees/index.js';
 import express from 'express';
+import {ValidateID} from '../middleware/index.js';
 const router = express.Router();
 router.route('/employees/getAll').get(getAllEmployees);
+router.route('/employees/getByID').get(ValidateID, getByID);
 export default router;
