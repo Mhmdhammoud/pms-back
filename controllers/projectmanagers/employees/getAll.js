19c84496ffde2c0b98c175b4869d4c92507d5836
Mhmd.H <dr.co0obra@hotmail.com>
2021-03-06 13:34:04 +0200

    removed password from route

--- controllers/projectmanagers/employees/getAll.js
+++ controllers/projectmanagers/employees/getAll.js
@@ -1,7 +1,7 @@
 import {Employee} from '../../../models/index.js';
 export default async (req, res) => {
 	try {
-		const ALL_EMPLOYEES = await Employee.find({}).select('-__v)');
+		const ALL_EMPLOYEES = await Employee.find({}).select('-__v -password');
 		return res.status(200).json({
 			status: 'Success',
 			message: 'All employees were fetched successfully',
