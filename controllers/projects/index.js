3b2509b007fb6e41185db8fb4f38b7687188c308
Mhmd.H <dr.co0obra@hotmail.com>
2021-03-09 15:05:29 +0200

    add comment route

--- controllers/projects/index.js
+++ controllers/projects/index.js
@@ -1,2 +1,3 @@
-export { default as getAll } from './getAll.js';
-export { default as getByID } from './getByID.js';
+export {default as getAll} from './getAll.js';
+export {default as getByID} from './getByID.js';
+export {default as addComment} from './addComment.js';
--- routes/generealRoutess.js
+++ routes/generealRoutess.js
@@ -3,10 +3,13 @@ import {
 	getByID,
 } from '../controllers/projectmanagers/employees/index.js';
 import {getTasks} from '../controllers/employee/index.js';
+import {addComment} from '../controllers/projects/index.js';
 import express from 'express';
 import {ValidateID} from '../middleware/index.js';
 const router = express.Router();
 router.route('/employees/getAll').get(getAllEmployees);
 router.route('/employees/getByID').get(ValidateID, getByID);
 router.route('/employees/getTasks').get(getTasks);
+router.route('/projects/addComment').put(addComment);
+
 export default router;
