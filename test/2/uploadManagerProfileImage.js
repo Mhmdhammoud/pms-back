ddf4ba82c5ec0934f516c8484296fa91fd84b66f
Mhmd.H <dr.co0obra@hotmail.com>
2021-03-21 21:57:48 +0200

    added upload tests

--- test/2/uploadManagerProfileImage.js
+++ test/2/uploadManagerProfileImage.js
@@ -3,6 +3,35 @@ import {expect} from 'chai';
 import request from 'supertest';
 import app from '../../Server.js';
 import connectDB from '../../config/db.js';
+import AWS from 'aws-sdk';
+async function emptyS3Directory(dir) {
+	const s3 = new AWS.S3({
+		accessKeyId: process.env.AWS_ACESS_KEY_ID,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	});
+
+	const listParams = {
+		Bucket: process.env.BUCKET_NAME,
+		Prefix: dir,
+	};
+
+	const listedObjects = await s3.listObjectsV2(listParams).promise();
+
+	if (listedObjects.Contents.length === 0) return;
+
+	const deleteParams = {
+		Bucket: process.env.BUCKET_NAME,
+		Delete: {Objects: []},
+	};
+
+	listedObjects.Contents.forEach(({Key}) => {
+		deleteParams.Delete.Objects.push({Key});
+	});
+
+	await s3.deleteObjects(deleteParams).promise();
+
+	if (listedObjects.IsTruncated) await emptyS3Directory(dir);
+}
 describe('Uploading a manager profile image ', () => {
 	before((done) => {
 		connectDB()
@@ -10,9 +39,11 @@ describe('Uploading a manager profile image ', () => {
 			.catch((err) => done(err));
 	});
 	it('FAIL, Upload manager profile image without Manager Login', (done) => {
+		const buffer = Buffer.from('../storm.jpg');
 		request(app)
 			.put('/api/v1/manager/profile/image')
-			.attach('profileImage', '../storm.jpg')
+			.attach('profileImage', buffer, 'storm.jpg')
+			.set({'Content-Type': 'multipart'})
 			.then(({body}) => {
 				expect(body).to.contain.property('message');
 				expect(body).to.contain.property('error');
@@ -20,11 +51,13 @@ describe('Uploading a manager profile image ', () => {
 				expect(body.error).to.equal(
 					'This route needs a higher clearance level'
 				);
+				expect(403);
 				done();
 			})
 			.catch((err) => done(err));
 	});
 	it('FAIL, Upload manager profile image with Employee Login', (done) => {
+		const buffer = Buffer.from('../storm.jpg');
 		request(app)
 			.post('/api/v1/employee/login')
 			.send({
@@ -37,8 +70,9 @@ describe('Uploading a manager profile image ', () => {
 				expect(body.status).to.equal('success');
 				request(app)
 					.put('/api/v1/manager/profile/image')
-					.attach('profileImage', '../storm.jpg')
+					.attach('profileImage', buffer, 'storm.jpg')
 					.set({Authorization: `Bearer ${body.token}`})
+					.set({'Content-Type': 'multipart'})
 					.then(({body}) => {
 						expect(body).to.contain.property('message');
 						expect(body).to.contain.property('error');
@@ -51,6 +85,7 @@ describe('Uploading a manager profile image ', () => {
 			.catch((err) => done(err));
 	});
 	it('PASS,  Upload manager profile image with Manager Login', (done) => {
+		const buffer = Buffer.from('../storm.jpg');
 		request(app)
 			.post('/api/v1/manager/login')
 			.send({
@@ -63,13 +98,16 @@ describe('Uploading a manager profile image ', () => {
 				expect(body.status).to.equal('success');
 				request(app)
 					.put('/api/v1/manager/profile/image')
-					.attach('profileImage', '../storm.jpg')
+					.attach('profileImage', buffer, 'test-image.jpg')
 					.set({Authorization: `Bearer ${body.token}`})
-					.then(({body}) => {
-						expect(body).to.contain.property('imageSrc');
-						expect(body).to.contain.property('status');
-						expect(body.status).to.equal('Success');
-						done();
+					.set({'Content-Type': 'multipart'})
+					.then(({body: uploadBody}) => {
+						expect(uploadBody).to.contain.property('imageSrc');
+						expect(uploadBody).to.contain.property('status');
+						expect(uploadBody.status).to.equal('Success');
+						emptyS3Directory(`PMS/manager/${body.id}`).then(() => {
+							done();
+						});
 					})
 					.catch((err) => done(err));
 			})
